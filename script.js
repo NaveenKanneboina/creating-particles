e2875e549a26b70ca2da43a9f7429b9b09f39dd1
Naveen Kumar Kanneboina <naveenkanneboian1303@gmail.com>
2021-01-02 22:52:26 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+let particleArray;
